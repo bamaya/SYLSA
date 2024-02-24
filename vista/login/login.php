7d4ef3360ef5718fc40f3a18cd3669cd391c9d14
Byron Amaya <60157235+bamaya@users.noreply.github.com>
2024-02-24 10:53:34 -0600

    Update

--- vista/login/login.php
+++ vista/login/login.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/style2.css">
     <link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
     <!-- <link rel="stylesheet" href="css/all.min.css"> -->
     <!-- <link rel="stylesheet" href="css/fontawesome.min.css"> -->
