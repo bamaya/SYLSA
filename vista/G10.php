0e9ae12b8a7efaa0e480333fbf380a0866bd911f
Byron Amaya <60157235+bamaya@users.noreply.github.com>
2024-03-06 22:24:49 -0600

    Update asistencia

---
+++ vista/G10.php
@@ -0,0 +1,77 @@
+<?php
+   session_start();
+   if (empty($_SESSION['nombre']) and empty($_SESSION['apellido'])) {
+       header('location:login/login.php');
+  }
+
+?>
+
+<!-- primero se carga el topbar -->
+<?php require('./layout/topbar.php'); ?>
+<!-- luego se carga el sidebar -->
+<?php require('./layout/sidebar.php'); ?>
+
+<!-- inicio del contenido principal -->
+<div class="page-content">
+
+    <h4 class="text-center text-secondary">LISTA DE EMPLEADOS</h4>
+
+    <?php
+    include('../modelo/conexion.php');
+    $sql=$conexion->query("SELECT * FROM u484202321_sylsa_personal.empleados WHERE GRUPO_TRABAJO = 'G10 PAV'");
+    ?>
+
+    <table class="table table-bordered table-hover col-12" id="example">
+  <thead>
+    <tr>
+      <th scope="col">DUI</th> 
+      <th scope="col">NOMBRE</th>     
+      <th scope="col">CARGO</th>
+      <th scope="col">ISSS</th>
+      <th scope="col">AFP</th>
+      <th scope="col">TELEFONO</th>
+      <th scope="col">GRUPO</th>
+      <th scope="col">FECHA DE INGRESO</th>
+      <th scope="col">SEXO</th>
+      <th scope="col">FECHA DE NACIMIENTO</th>
+      <th scope="col">DIRECCION</th> 
+      <th scope="col">CUENTA BANCARIA</th>
+      <th scope="col">CONTACTO DE EMERGENCIA</th>
+      <th scope="col">TELEFONO DE EMERGENCIA</th>
+      <th scope="col">PARENTESCO</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+    
+    while ($datos=$sql->fetch_object()) { ?>
+          <tr>
+      <td><?=$datos->DUI ?></td>
+      <td><?=$datos->NOMBRE_COMPLETO ?></td>
+      <td><?=$datos->CARGO ?></td>
+      <td><?=$datos->ISSS ?></td>
+      <td><?=$datos->AFP ?></td>
+      <td><?=$datos->TELEFONO ?></td>
+      <td><?=$datos->GRUPO_TRABAJO ?></td>
+      <td><?=$datos->FECHA_INGRESO ?></td>
+      <td><?=$datos->SEXO ?></td>
+      <td><?=$datos->FECHA_NACIMIENTO ?></td>
+      <td><?=$datos->DIRECCION ?></td>
+      <td><?=$datos->CUENTA_BANCARIA ?></td>
+      <td><?=$datos->CONTACTO_EMERGENCIA ?></td>
+      <td><?=$datos->TELEFONO_EMERGENCIA ?></td>
+      <td><?=$datos->PARENTESCO ?></td>
+    </tr>
+    <?php
+    }
+    ?>
+  </tbody>
+</table>
+
+</div>
+</div>
+<!-- fin del contenido principal -->
+
+
+<!-- por ultimo se carga el footer -->
+<?php require('./layout/footer.php'); ?>
--- vista/layout/sidebar.php
+++ vista/layout/sidebar.php
@@ -9,37 +9,33 @@
                     </a>
                 </li>
 
-                <li class="red">
-                    <a href="asistencia.php" class="">
-                        <img src="../public/img-inicio/programar.png" class="img-inicio" alt="">
-                        <!-- <i class="fas fa-house-user"></i> -->
-                        <span class="lbl">ASISTENCIAS</span>
-                    </a>
-                </li>
-
                 <li class="grey with-sub">
                     <span>
                         <img src="../public/img-inicio/programar.png" class="img-inicio" alt="">
                         <!-- <i class="fas fa-sort-amount-up-alt"></i> -->
-                        <span class="lbl">CITAS</span>
+                        <span class="lbl">ASISTENCIAS</span>
                     </span>
                     <ul>
                         <li>
-                            <a href="" class="">
-                                <i class="fas fa-plus-square icono-submenu"></i>
-                                <span class="lbl">Registrar cita</span>
+                            <a href="G5.php" class="">
+                                <i class="fas fa-th-list icono-submenu"></i>
+                                <span class="lbl">Asistencia G5 PAV</span>
                             </a>
                         </li>
                         <li>
-                            <a href="" class="">
+                            <a href="G10.php" class="">
                                 <i class="fas fa-th-list icono-submenu"></i>
-                                <span class="lbl">Lista de citas</span>
+                                <span class="lbl">Asistencia G10 PAV</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="G11.php" class="">
+                                <i class="fas fa-th-list icono-submenu"></i>
+                                <span class="lbl">Asistencia G11 NOPAV</span>
                             </a>
                         </li>
                     </ul>
                 </li>
-
-                
 
                 <li class="red">
                     <a href="acerca.php" class="">
